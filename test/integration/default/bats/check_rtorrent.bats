199694371e88c577805d55f8905e9c2c691aaae4
Chris Down <chris@chrisdown.name>
2014-11-09 19:55:14 +0000

    tests: Use serverspec instead of bats

--- test/integration/default/bats/check_rtorrent.bats
+++
@@ -1,7 +0,0 @@
-@test "Check rtorrent is running" {
-  service rtorrent status
-}
-
-@test "Check that rtorrent is listening on port" {
-  netstat -lnptu | grep '0.0.0.0:1337.*[[:blank:]]\+LISTEN.*[[:blank:]]\+[[:digit:]]\+/rtorrent'
-}
---
+++ test/integration/default/serverspec/rtorrent_spec.rb
@@ -0,0 +1,13 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe 'rtorrent' do
+  it 'is listening on port 1337' do
+    expect(port(1337)).to be_listening
+  end
+
+  it 'has a running service' do
+    expect(service('rtorrent')).to be_running
+  end
+end
